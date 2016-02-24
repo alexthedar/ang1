f47be59854bf027f6167f5ce6e6080b1e7296de3
alexthedar <alex.castaneda@gmail.com>
2016-02-23 20:47:55 -0800

    added routing, 2 controllers and ng repeats

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 
-var app = angular.module('computer', ['ngRoute']);
+var app = angular.module('computer', ['ngRoute'])
 
-app.config(['$routeProvider', function($routeProvider){
-  $routeProvider.when('/', {
+.config(['$routeProvider', function($routeProvider){
+  $routeProvider.when('/main', {
       templateUrl: 'main.html',
       controller: 'MainCtrl'
-  }).
-  otherwise({redirectTo:'/'})
-}]);
+  })
+  .when('/about', {
+      templateUrl: 'about.html',
+      controller: 'MainCtrl'
+  })
+  .when('/services', {
+      templateUrl: 'services.html',
+      controller: 'ServicesCtrl'
+  })
+  .when('/contact', {
+      templateUrl: 'contact.html',
+      controller: 'ContactCtrl'
+  })
+  .otherwise({redirectTo:'/main'})
+}])
 
-app.controller('MainCtrl', [function(){
-  console.log('this is the main controller');
+.controller('MainCtrl', ['$scope',function($scope){
+
+}])
+.controller('ServicesCtrl', ['$scope', '$http', function($scope, $http){
+  $http.get('services.json').then(function(response){
+    $scope.services = response.data;
+  })
+}])
+.controller('ContactCtrl', ['$scope', '$http',function($scope, $http){
+
 }]);
